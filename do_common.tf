907e36da19eec300f9155557f6c1bffb167520b3
Alexandre P <1724149+zaggash@users.noreply.github.com>
2022-10-06 10:43:49 +0200

    Write priv key to a file

--- do_common.tf
+++ do_common.tf
@@ -6,7 +6,13 @@ resource "digitalocean_vpc" "infra-clusters" {
 resource "tls_private_key" "priv-key" {
   algorithm   = "RSA"
 }
+
 resource "digitalocean_ssh_key" "pub-key" {
   name        = "${var.prefix}-cluster-nodes-key"
   public_key  = tls_private_key.priv-key.public_key_openssh
+}
+
+resource "local_file" "id_rsa" {
+    content  = tls_private_key.priv-key.private_key_pem
+    filename = "id_rsa.pem"
 }
